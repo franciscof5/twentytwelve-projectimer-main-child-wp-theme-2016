c8d63227b3416e92c82a16b310e58285343234f3
Francisco Mat <francisco@f5sites.com>
2020-04-13 21:14:21 -0300

    f5sites smartlang recent posts widget sidebar

---
+++ sidebar.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * If no active widgets are in the sidebar, hide it completely.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+
+	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+		<div id="secondary" class="widget-area" role="complementary">
+			<?php #dynamic_sidebar( 'sidebar-1' ); ?>
+			<?php 
+			if(function_exists("smartlang_recent_posts_georefer_widget"))
+			smartlang_recent_posts_georefer_widget();
+			#the_widget("Recent_Posts_Widget_Extended", "limit=20"); 
+			?>
+		</div><!-- #secondary -->
+	<?php endif; ?>
